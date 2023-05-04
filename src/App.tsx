6485877f40475fc65b0f02ea617c53839473e866
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-05-04 19:22:42 +0400

    Header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+
+import Header from "./Components/Header";
+
+// import bgBottom from "./Images/bg-bottom.svg";
+import bgTop from "./Images/bg-top.svg";
+import { useState } from "react";
+
 function App() {
-  return;
+  const [toggle, setToggle] = useState<boolean>(true);
+  return (
+    <Container>
+      <Header toggle={toggle} setToggle={setToggle} />
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f7f7fe;
+  background-image: url(${bgTop});
+  background-repeat: no-repeat;
+  background-position: right -193px top 0;
+`;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+interface Props {
+  toggle: boolean;
+  setToggle: (toggle: boolean) => void;
+}
+
+export default function Header({ toggle, setToggle }: Props) {
+  const changeValue = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <Container>
+      <h1>Our Pricing</h1>
+      <div>
+        <h2>Annually</h2>
+        <label
+          style={{
+            justifyContent: toggle ? "start" : "end",
+          }}
+        >
+          <input type="checkbox" onClick={changeValue} />
+        </label>
+        <h2>Monthly</h2>
+      </div>
+    </Container>
+  );
+}
+const Container = styled.div`
+  width: 100%;
+  max-width: 327px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  align-items: center;
+  padding: 64px 44px 80px;
+  h1 {
+    font-size: 32px;
+    line-height: 39px;
+    text-align: center;
+    color: #6e728e;
+  }
+
+  div {
+    display: flex;
+    gap: 24px;
+    align-items: center;
+
+    h2 {
+      font-size: 15px;
+      line-height: 28px;
+      text-align: right;
+      color: #6e728e;
+      mix-blend-mode: normal;
+      opacity: 0.5;
+    }
+
+    label {
+      width: 56px;
+      height: 32px;
+      border-radius: 16px;
+      background: linear-gradient(135deg, #a2a7f0 0%, #696edd 100%);
+      display: flex;
+      align-items: center;
+      padding: 4px;
+
+      input {
+        appearance: none;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        background-color: #ffffff;
+      }
+    }
+  }
+`;
